b050f2b229de2506fcb4cd81d75e91c26fd19c78
WASSIM BOUHDID <HE201893@students.ephec.be>
2023-04-01 01:06:12 +0200

    projects presentation structure + contact form

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,21 +1,107 @@
 <template lang="">
-    <div id="front">
-    <h1>Portfolio</h1>
+
+  <div id="front">
+    <div style="position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);max-width: 90rem;width: 92%;">
+      <h1 id="titleCv">Portfolio</h1>
+      <h2>Mes projects</h2>
+    </div>
   </div>
 
-    <div class="container">
-      <button @click="say('hello')">Say hello</button>
-<button @click="username2()">get route</button>
+  <div class="container">
+    <div style="margin:55px;" class="personnalProject">
+      <div style="width:450px;margin:25px;">
+        <img src="#">
+      </div>
+      <div style="width:450px;margin:25px;">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div style="display:flex;margin:25px;">
+            <img src="#"><img src="#"><img src="#"><img src="#"><img src="#"><img src="#">
+          </div>
+        </div>
+        <Button>Voir le project</Button>
+      </div>
     </div>
+
+    <div style="margin:55px;" class="personnalProject">
+      <div style="width:450px;margin:25px;">
+        <img src="#">
+      </div>
+      <div style="width:450px;margin:25px;">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div style="display:flex;margin:25px;">
+            <img src="#"><img src="#"><img src="#"><img src="#"><img src="#"><img src="#">
+          </div>
+        </div>
+        <Button>Voir le project</Button>
+      </div>
+    </div>
+
+    <div style="margin:55px;" class="personnalProject">
+      <div style="width:450px;margin:25px;">
+        <img src="#">
+      </div>
+      <div style="width:450px;margin:25px;">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div style="display:flex;margin:25px;">
+            <img src="#"><img src="#"><img src="#"><img src="#"><img src="#"><img src="#">
+          </div>
+        </div>
+        <Button>Voir le project</Button>
+      </div>
+    </div>
+
+    <div style="margin:55px;" class="personnalProject">
+      <div style="width:450px;margin:25px;">
+        <img src="#">
+      </div>
+      <div style="width:450px;margin:25px;">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div style="display:flex;margin:25px;">
+            <img src="#"><img src="#"><img src="#"><img src="#"><img src="#"><img src="#">
+          </div>
+        </div>
+        <Button>Voir le project</Button>
+      </div>
+    </div>
+  </div>
+  <div>
+    <h2>Contact</h2>
+    <form>
+      <label>nom</label>
+      <input>
+      <label>prénom</label>
+      <input>
+      <label>adresse mail</label>
+      <input>
+      <label>Message</label>
+      <input>
+    </form>
+  </div>
 </template>
 <script>
-export default {
+  export default {
     name: 'PortfolioView',
-  components: {
-    
+    components: {
+
+    }
   }
-}
 </script>
 <style lang="">
-    
+
 </style>
