0ace2ed0da958dc3361aa248df23fa2d93942f8c
Bouhdid Wassim <64103126+WassimBouhdid@users.noreply.github.com>
2023-04-23 13:55:42 +0200

    Responciv (#5)

--- src/views/CurriculumView.vue
+++ src/views/CurriculumView.vue
@@ -1,16 +1,204 @@
 <template lang="">
-    <div>
-        CurriculumView
+
+  <div id="front">
+    <div class="mainTitle">
+      <h1 id="titleCv">Bouhdid Wassim</h1>
+      <div>
+        <h2>Developpeur</h2>
+        <p id="description"> Bonjour ! Je m'appelle Bouhdid Wassim. Je suis un développeur junior diplomé en technologies de l'informatique à Haute école de l'EPHEC à Louvain La Neuve. Je suis motivé pour prendre mes premiers pas dans le monde professionel et d'apprendre de nouvelles technologies. Sur mon site, vous pourrez avoir accès à mon cv qui se trouve ci-dessous et à mon <a href="/portfolio">portfolio</a>.</p>
+      </div>
+      <div id="socialWrapper">
+        <a href="/">
+          <div class="socialLink" ><img class="socialIcon" alt="facebook logo" src="../assets/facebook.png"></div>
+        </a>
+        <a href="/">
+          <div class="socialLink" ><img class="socialIcon" alt="linkedin logo" src="../assets/linkedin.png"></div>
+        </a>
+        <a href="/">
+          <div class="socialLink" ><img class="socialIcon" alt="instagram logo" src="../assets/instagram.png"></div>
+        </a>
+        <a href="/">
+          <div class="socialLink" ><img class="socialIcon" alt="twitter logo" src="../assets/twitter.png"></div>
+        </a>
+        <a href="/">
+          <div class="socialLink" ><img class="socialIcon" alt="github logo" src="../assets/github.png"></div>
+        </a>
+        <a href="/portfolio">
+          <div id="projectLink" ><span id="projectText">Mes
+              projects</span></div>
+        </a>
+      </div>
     </div>
+  </div>
+
+  <div class="container" id="curriculumCardContainer">
+    <div id="exp">
+      <h1>Bouhdid Wassim</h1>
+      <div>
+        <h3>Études/Formations</h3>
+        <div id="formation">
+          <div id="dateFromation1">2018 Bruxelles - Belgique</div>
+          <div id="descriptionFormation1">Diplôme CESS à l'Ecole Notre Dames des Champs</div>
+          <div id="dateFromation2">2019 Belgique</div>
+          <div id="descriptionFormation2">Formation d’initiation au développement web (CSS, JavaScript, HTML) Ephec
+            (Technofutur)</div>
+          <div id="dateFromation3">2019 - 2 Février 2023</div>
+          <div id="descriptionFormation3">Bachelier en technologie de l’informatique Ephec , Louvain la-neuve</div>
+        </div>
+      </div>
+      <div>
+        <h3>Exprériences professionelles</h3>
+        <div id="experience">
+          <div id="dateExp1">Pâques/Eté 2019 et 2020 Bruxelles-Belgique</div>
+          <div id="descriptionExp1">Money International
+            Guichetier
+            Tâches réalisées :<br>
+            <ul>
+              <li>Création et envoie de mandat à l’étranger</li>
+              <li>Gestion de la caisse</li>
+            </ul>
+          </div>
+          <div id="dateExp2">Pâques/Eté 2019 et 2020 Bruxelles-Belgique</div>
+          <div id="descriptionExp2">ApConsulting | Campus Solvay
+            Technicien
+            Tâches réalisées :<br>
+            <ul>
+              <li>Montage d’écran</li>
+              <li>Installation de dockings stations</li>
+              <li>Cablage</li>
+              <li>Testing</li>
+            </ul>
+
+          </div>
+          <div id="dateExp3">2019 Bruxelles-Belgique</div>
+          <div id="descriptionExp3">ApConsulting | Mastercard Waterloo
+            Technicien
+            Tâches réalisées :<br>
+            <ul>
+              <li>Installation des bureaux</li>
+              <li>Cablage</li>
+              <li>Testing des installations</li>
+            </ul>
+          </div>
+
+          <div id="dateExp4">2022 Wavre Février -> Mai</div>
+          <div id="descriptionExp4">Stage de ApConsulting
+            Dévellopeur web
+            Tâches réalisées :<br>
+            <ul>
+              <li>Création de sites wordpress</li>
+              <li>Création de sites avec NodeJS</li>
+              <li>Web3</li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+      <div>
+        <h3>Langues</h3>
+
+        <ul>
+          <li class="listLangue"><span class="langueNom">Français</span>
+            <div class="lvlMax">
+              <div id="francais"></div>
+            </div> <span class="lvl">Niveau : Langue maternelle</span>
+          </li>
+          <li class="listLangue"><span class="langueNom">Néérlandais</span>
+            <div class="lvlMax">
+              <div id="ndls"></div>
+            </div> <span class="lvl">Niveau : B1</span>
+          </li>
+          <li class="listLangue"><span class="langueNom">Anglais</span>
+            <div class="lvlMax">
+              <div id="anglais"></div>
+            </div> <span class="lvl">Niveau : B1</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div id="data">
+      <div id="imageWrapper">
+        <img class="img-fluid" id="imgProfile"
+          src="../assets/imgProfile.jpg">
+      </div>
+      <div id="contact">
+        <h5>Contact</h5>
+        <ul>
+          <li class="contactInfo"> <img src="../assets/adress.png"> Vandenpeereboomstraat n°318</li>
+          <li class="contactInfo"><img src="../assets/email.png"> Bouhdid.wassim@hotmail.com</li>
+          <li class="contactInfo"><img src="../assets/phone-call.png"> (+32)479/08 64 28</li>
+        </ul>
+      </div>
+      <hr>
+      <div id="technologies">
+        <h5>Les technologies utilisées/apprises</h5>
+        <ul>
+          <li> NodeJS </li>
+          <li> JS/HTML/CSS </li>
+          <li> Angular </li>
+          <li> VueJS </li>
+          <li> Python </li>
+          <li> Kotlin </li>
+          <li> PHP </li>
+          <li> Symphony </li>
+        </ul>
+      </div>
+      <hr>
+      <div id="skills">
+        <h5>Compétences</h5>
+        <ul>
+          <li> Polyvalence </li>
+          <li> Ponctualité </li>
+          <li> Esprit d’équipe </li>
+          <li> Gestion du stress</li>
+        </ul>
+      </div>
+      <hr>
+
+      <div id="interrests">
+        <h5>Intérêts</h5>
+        <ul>
+          <li> Jeu vidéo </li>
+          <li> Manga </li>
+          <li> Sports </li>
+          <li> Création de jeux vidéo </li>
+          <li> Le développement informatique </li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+
 </template>
 <script>
-export default {
+  import {
+    useRouter
+  } from 'vue-router';
+  export default {
     name: 'CurriculumView',
-  components: {
-    
+    components: {
+
+    },
+    computed: {
+      username() {
+
+      },
+    },
+    methods: {
+      getRouteParams() {
+        console.log(this.$route.name)
+      },
+      say(message) {
+        alert(message)
+      }
+
+    },
+    beforeMount() {
+
+    },
+    setup() {}
   }
-}
 </script>
 <style lang="">
-    
+
 </style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,16 +1,131 @@
 <template lang="">
-    <div>
-        PortfolioView
+
+  <div id="front">
+    <div class="mainTitle">
+      <h1 id="titleCv">Portfolio</h1>
+      <h2>Mes projets</h2>
     </div>
+  </div>
+
+  <div class="container">
+    <div class="personnalProject">
+      <div class="projectImageWrapper">
+        <img class="img-fluid" src="../assets/ordinateur.png">
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div class="techLogoWrapper">
+            <img class="img-fluid techLogo" src="../assets/vue-js-logo.png"><img class="img-fluid techLogo" src="../assets/angular-logo.png"><img class="img-fluid techLogo" src="../assets/nodejs.png"><img class="img-fluid techLogo" src="../assets/mysql.png"><img class="img-fluid techLogo" src="../assets/kotlin.png"><img class="img-fluid techLogo" src="../assets/symfony.png">
+          </div>
+        </div>
+        <Button>Voir le projet</Button>
+      </div>
+    </div>
+
+    <div class="personnalProject">
+      <div class="projectImageWrapper">
+        <img class="img-fluid" src="../assets/ordinateur.png">
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div class="techLogoWrapper">
+            <img class="img-fluid techLogo" src="../assets/vue-js-logo.png"><img class="img-fluid techLogo" src="../assets/angular-logo.png"><img class="img-fluid techLogo" src="../assets/nodejs.png"><img class="img-fluid techLogo" src="../assets/mysql.png"><img class="img-fluid techLogo" src="../assets/kotlin.png"><img class="img-fluid techLogo" src="../assets/symfony.png">
+          </div>
+        </div>
+        <Button>Voir le projet</Button>
+      </div>
+    </div>
+
+    <div class="personnalProject">
+      <div class="projectImageWrapper">
+        <img class="img-fluid" src="../assets/ordinateur.png">
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div class="techLogoWrapper">
+            <img class="img-fluid techLogo" src="../assets/vue-js-logo.png"><img class="img-fluid techLogo" src="../assets/angular-logo.png"><img class="img-fluid techLogo" src="../assets/nodejs.png"><img class="img-fluid techLogo" src="../assets/mysql.png"><img class="img-fluid techLogo" src="../assets/kotlin.png"><img class="img-fluid techLogo" src="../assets/symfony.png">
+          </div>
+        </div>
+        <Button>Voir le projet</Button>
+      </div>
+    </div>
+
+    <div class="personnalProject">
+      <div class="projectImageWrapper">
+        <img class="img-fluid" src="../assets/ordinateur.png">
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div class="techLogoWrapper">
+            <img class="img-fluid techLogo" src="../assets/vue-js-logo.png"><img class="img-fluid techLogo" src="../assets/angular-logo.png"><img class="img-fluid techLogo" src="../assets/nodejs.png"><img class="img-fluid techLogo" src="../assets/mysql.png"><img class="img-fluid techLogo" src="../assets/kotlin.png"><img class="img-fluid techLogo" src="../assets/symfony.png">
+          </div>
+        </div>
+        <Button>Voir le projet</Button>
+      </div>
+    </div>
+
+    <div class="personnalProject">
+      <div class="projectImageWrapper">
+        <img class="img-fluid" src="../assets/ordinateur.png">
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div class="techLogoWrapper">
+            <img class="img-fluid techLogo" src="../assets/vue-js-logo.png"><img class="img-fluid techLogo" src="../assets/angular-logo.png"><img class="img-fluid techLogo" src="../assets/nodejs.png"><img class="img-fluid techLogo" src="../assets/mysql.png"><img class="img-fluid techLogo" src="../assets/kotlin.png"><img class="img-fluid techLogo" src="../assets/symfony.png">
+          </div>
+        </div>
+        <Button>Voir le projet</Button>
+      </div>
+    </div>
+
+    <div class="personnalProject">
+      <div class="projectImageWrapper">
+        <img class="img-fluid" src="../assets/ordinateur.png">
+      </div>
+      <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cupiditate animi, nostrum ratione neque
+          voluptatum distinctio voluptatem est odio accusantium, ex repellat explicabo expedita ipsum quidem rem aliquam
+          doloribus fugit.</p>
+        <div>
+          <h4>les technologies utilisées</h4>
+          <div class="techLogoWrapper">
+            <img class="img-fluid techLogo" src="../assets/vue-js-logo.png"><img class="img-fluid techLogo" src="../assets/angular-logo.png"><img class="img-fluid techLogo" src="../assets/nodejs.png"><img class="img-fluid techLogo" src="../assets/mysql.png"><img class="img-fluid techLogo" src="../assets/kotlin.png"><img class="img-fluid techLogo" src="../assets/symfony.png">
+          </div>
+        </div>
+            <Button>Voir le projet</Button>
+      </div>
+    </div>
+  </div>
+
 </template>
 <script>
-export default {
+  export default {
     name: 'PortfolioView',
-  components: {
-    
+    components: {
+
+    }
   }
-}
 </script>
 <style lang="">
-    
+
 </style>
